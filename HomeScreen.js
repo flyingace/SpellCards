a760b6f5a2e395a9e032e45a89d1942a0293ec08
flyingace <dc@davidcameron.biz>
2018-10-21 00:00:36 -0400

    More missing files

---
+++ HomeScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Button, StyleSheet, Text, View,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text>Spell Cards</Text>
+        <Button
+          title="All Spells"
+          onPress={() => navigate('allSpellz')}
+        />
+        <Button
+          title="Add Spells"
+          onPress={() => navigate('addSpells')}
+        />
+        <Button
+          title="Spell Books"
+          onPress={() => navigate('spellBooks')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
