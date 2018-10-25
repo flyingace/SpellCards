2edd8d3c60e9c1631217d77389c49cd6a1673ae5
flyingace <dc@davidcameron.biz>
2018-10-25 17:13:52 -0400

    Added SpellCard component, imported spell data.

--- components/AllSpells.js
+++ components/AllSpells.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import allSpells from '../data/5e-SRD-Spells.json';
+import SpellCard from './SpellCard';
 
 export default class AllSpells extends React.Component {
+  generateSpells = () => {
+    return allSpells.map(spell => <SpellCard spellData={spell} />);
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text>All Spells</Text>
+        {this.generateSpells()}
       </View>
     );
   }
